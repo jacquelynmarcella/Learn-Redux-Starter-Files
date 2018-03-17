00df842a827948bce0308910dcee0eae5c0dc467
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-03-16 17:08:58 -0700

    Add/delete comments from state through redux

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,37 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
+	switch(action.type){
+		case 'ADD_COMMENT':
+		// return state with new comment
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}]
+		case 'REMOVE_COMMENT':
+		// return the new state without deleted comment
+			return [
+			// from the start to the one we want to delete
+				...state.slice(0, action.i),
+				// after deleted one to end
+				...state.slice(action.i + 1)
+			]
+		default:
+			return state;
+	}
 	return state;
 }
+
+function comments(state = [], action) {
+	console.log(state,action);
+	if(typeof action.postId !== 'undefined') {
+		return {
+			// take current state
+			...state,
+			// overwrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
+	return state;
+}
+
 
 export default comments;
